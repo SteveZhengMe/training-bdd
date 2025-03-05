be232987b89ffc88bbae1d6243f2dc07663b6dd1
Steve Zheng <steve.zheng@gmail.com>
2025-03-05 19:03:47 +0000

    Realtime inference BDD bugs are fixed. Now done.

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,7 +1,15 @@
 import sys
 import os
+import logging
+
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../src")))
 import example
 import etl
 import batch_infer
+
+logging.getLogger().setLevel(logging.ERROR)
+logging.basicConfig(
+    filename="log.log",
+    format="%(asctime)s %(levelname)s %(name)s %(message)s",
+)
--- tests/realtime_infer/sce_realtime.py
+++ tests/realtime_infer/sce_realtime.py
@@ -1,5 +1,7 @@
 import os
 import requests
+import logging
+
 from behave import given, when, then
 from src.batch_infer import BatchInfer
 
@@ -8,7 +10,7 @@ from src.batch_infer import BatchInfer
     "the endpoint base url is '{endpoint_base_url}',and the model context path is '{context_path}'"
 )
 def configure_endpoint(context, endpoint_base_url, context_path):
-    context.endpoint_url = endpoint_base_url + "/" + context_path + "/"
+    context.endpoint_url = endpoint_base_url + context_path + "/"
 
 
 @when("I send the {category_name}, {name}, {status} to the model")
@@ -18,17 +20,16 @@ def send_data_to_model(context, category_name, name, status):
     context.response = requests.post(
         context.endpoint_url, json=request_body, headers=headers
     )
+    logging.debug(context.response.url)
 
 
 @then("The endpoint should return the response with status code '{http_status_code}'")
 def check_response_status_code(context, http_status_code):
-    # print(context.response)
-    # assert context.response.status_code == int(http_status_code)
+    assert context.response.status_code == int(http_status_code)
 
 
 @then("The response should have the key '{new_id}', and it is a number")
 def validate_new_id(context, new_id):
-    # response_data = context.response.json()
-    # assert new_id in response_data
-    # assert isinstance(response_data[new_id], int)
-    pass
+    response_data = context.response.json()
+    assert new_id in response_data
+    assert isinstance(response_data[new_id], int)
